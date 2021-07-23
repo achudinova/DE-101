6185d419dd632ecd614e4f529480a07cf1b19de8
Anastasia <anastasiushka995@gmail.com>
2021-07-23 12:35:47 +0200

    Module 4. Pentaho objects

---
+++ Module04/DE - 101 Lab 4.4/pentaho_objects/sheduling_job.bat
@@ -0,0 +1,1 @@
+"C:\pdi-ce-9.1.0.0-324\data-integration\Kitchen.bat" /file:"C:\Users\User\Desktop\introduction_pentaho\final_job.kjb" /level:Basic
---
+++ Module04/sources/introduction_pentaho/import.ps1
@@ -0,0 +1,2 @@
+#!powershell
+curl -O C:/Users/homa9/Documents/GitHub/DE-101/Module04/DE - 101 Lab 4.4/files/sample-superstore-shell.xls https://github.com/Data-Learn/data-engineering/raw/master/DE-101%20Modules/Module01/DE%20-%20101%20Lab%201.1/Sample%20-%20Superstore.xls
